84a9583b2ddf8e152ea546803ab460269dd2c14d
farid <dika59@hotmail.fr>
2015-10-04 10:56:28 +0200

    Formulaire d'upload fonctionnel avec rangement des fichiers dans web/uploads/img/

--- src/EP/UploadBundle/Controller/UploadController.php
+++ src/EP/UploadBundle/Controller/UploadController.php
@@ -3,7 +3,9 @@
 namespace EP\UploadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use EP\UploadBundle\Entity\Files;
+use EP\UploadBundle\Form\FilesType;
 
 class UploadController extends Controller
 {
@@ -13,14 +15,39 @@ class UploadController extends Controller
         // return new Response("Hello World !");
     }
 
- //    public function uploadAction()
- //    {
-	//     if ($form->isValid()) {
-	//         $someNewFilename = ...
+    public function uploadAction(Request $request)
+    {
+   	// On crée un objet Advert
+    $file = new Files();
+    // On crée le FormBuilder grâce au service form factory
+    // $form = $this->get('form.factory')->create(new FilesType,$file);
+	$form = $this->createForm(new FilesType(), $file);
+    // On fait le lien Requête <-> Formulaire
+    // À partir de maintenant, la variable $file contient les valeurs entrées dans le formulaire par le visiteur
+    $form->handleRequest($request);
 
-	//         $form['attachment']->getData()->move($dir, $someNewFilename);
+    // On vérifie que les valeurs entrées sont correctes
+    // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+    if ($form->isValid()) {
+      // On l'enregistre notre objet $advert dans la base de données, par exemple
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($file);
+      $em->flush();
 
-	//         // ...
-	//     }
-	// }
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+      // On redirige vers la page de visualisation de l'annonce nouvellement créée
+      // return $this->redirect($this->generateUrl('ep_upload_home', array('id' => $file->getId())));
+    }
+
+    // À ce stade, le formulaire n'est pas valide car :
+    // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+    // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
+
+    // On passe la méthode createView() du formulaire à la vue
+    // afin qu'elle puisse afficher le formulaire toute seule
+    return $this->render('EPUploadBundle:Upload:form.html.twig', array(
+      'form' => $form->createView(),'file'=>$file
+    ));
+	}
 }
---
+++ src/EP/UploadBundle/Form/FilesType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace EP\UploadBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class FilesType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('file','file')
+            ->add('save','submit')  
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'EP\UploadBundle\Entity\Files'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ep_uploadbundle_files';
+    }
+}
